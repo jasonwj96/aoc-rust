a8617ff317ffed239d053475802bbb418c6c16cc
Jason Wedderburn <jasonwj96@gmail.com>
2025-10-06 23:42:37 -0500

    Day 1 solved

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,1 @@
 pub mod year2024;
-
-pub trait Solution {
-    fn parse(input: &str) -> Self
-        where
-            Self: Sized;
-    fn part1(&self) -> String;
-    fn part2(&self) -> String;
-}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use aoc_rust::year2024::day01;
+use std::env;
+use std::io::{self};
+
+
+fn main() -> io::Result<()> {
+    let cwd = env::current_dir()?;
+    println!("Current directory: {}", cwd.display());
+
+    let input_path = cwd.join("input/year2024/day01.txt");
+
+    let result = day01::part1(&input_path)?;
+    println!("Part 1 result: {}", result);
+
+    let result = day01::part2(&input_path)?;
+    println!("Part 2 result: {}", result);
+
+    Ok(())
 }
